c9cd28317fe90b82d77d193e03c00aeef07fc78d
Patryk Kubala <patrykkubala1999@gmail.com>
2020-05-19 21:42:55 +0200

    changes to menu pre media

--- src/components/Input.js
+++ src/components/Input.js
@@ -10,39 +10,37 @@ const InputWraper = styled.nav`
   justify-content: space-between;
   align-items: center;
   min-width: 400px;
-  width: 60vh;
   flex-wrap: wrap;
-  select:focus,
-  textarea:focus,
-  input:focus {
-    outline: none;
-  }
-  input {
-    width: 65%;
-    box-sizing: border-box;
-    border: 2px solid ${({ theme }) => theme.colors.dark};
-    border-radius: 20px;
-    font-size: 18px;
-    background-color: ${({ theme }) => theme.colors.white};
-    background-image: url(${zoom});
-    outline: none;
-    background-position: 10px 10px;
-    background-repeat: no-repeat;
-    padding: 13px 0px 12px 40px;
-  }
+`;
 
-  button {
-    min-width: 30%;
-    box-sizing: border-box;
-    border: 2px solid ${({ theme }) => theme.colors.dark};
-    border-radius: 20px;
-    font-size: 18px;
-    background-color: ${({ theme }) => theme.colors.white};
-    padding: 12px 5px;
+const InputArea = styled.input`
+  width: 65%;
+  box-sizing: border-box;
+  border: 2px solid ${({ theme }) => theme.colors.dark};
+  border-radius: 20px;
+  font-size: 18px;
+  background-color: ${({ theme }) => theme.colors.white};
+  background-image: url(${zoom});
+  outline: none;
+  background-position: 10px 10px;
+  background-repeat: no-repeat;
+  padding: 13px 0px 12px 40px;
+  :focus {
     outline: none;
-    :hover {
-      outline: none;
-    }
+  }
+`;
+
+const Button = styled.button`
+  min-width: 30%;
+  box-sizing: border-box;
+  border: 2px solid ${({ theme }) => theme.colors.dark};
+  border-radius: 20px;
+  font-size: 18px;
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: 12px 5px;
+  outline: none;
+  :hover {
+    outline: none;
   }
 `;
 
@@ -51,8 +49,8 @@ const Input = () => {
   return (
     <form onSubmit={handleSubmit}>
       <InputWraper>
-        <input onChange={handleSearch} type="text" placeholder="Search.." />
-        <button type="submit">Search</button>
+        <InputArea onChange={handleSearch} type="text" placeholder="Search.." />
+        <Button type="submit">Search</Button>
       </InputWraper>
     </form>
   );

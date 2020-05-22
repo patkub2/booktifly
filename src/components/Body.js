3f33b7b3f5bd49eb9eafc6d02f8c8440890a93d7
Patryk Kubala <patrykkubala1999@gmail.com>
2020-05-22 20:25:56 +0200

    styled body

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,7 @@ import { SearchContext } from "./searchContext";
 import nobook from "../img/nobook.png";
 
 const BodyWrapper = styled.div`
-  border: 1px solid red; /* RED*/
+  /*border: 1px solid red; /* RED*/
   width: 100%;
   min-height: 80vh;
   padding: 0px 50px;
@@ -16,7 +16,7 @@ const BodyWrapper = styled.div`
 `;
 
 const Book = styled.div`
-  border: 1px solid green; /* GREEN*/
+  /*border: 5px solid white; /* GREEN*/
   display: flex;
   width: 26vh;
   height: 36vh;
@@ -25,43 +25,6 @@ const Book = styled.div`
   align-items: center;
   justify-content: center;
   border-radius: 20px;
-  .container {
-    position: relative;
-    height: 36vh;
-    width: 26vh;
-    border-radius: 20px;
-  }
-
-  .overlay {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 100%;
-    width: 100%;
-    opacity: 0;
-    transition: 0.5s ease;
-    background-color: #008cba;
-
-    border-radius: 20px;
-  }
-
-  .container:hover .overlay {
-    opacity: 1;
-  }
-
-  .text {
-    color: white;
-    font-size: 20px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    -webkit-transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    text-align: center;
-  }
 `;
 
 const Img = styled.img`
@@ -69,7 +32,7 @@ const Img = styled.img`
   width: 100%;
   max-height: 100%;
   height: 100%;
-
+  border: 5px solid ${({ theme }) => theme.colors.white}; /* GREEN*/
   border-radius: 20px;
 `;
 
@@ -83,7 +46,7 @@ const Overlay = styled.div`
   width: 100%;
   opacity: 0;
   transition: 0.5s ease;
-  background-color: #008cba;
+  background-color: ${({ theme }) => theme.colors.white};
 
   border-radius: 20px;
 `;
@@ -94,12 +57,12 @@ const Cont = styled.div`
   width: 26vh;
   border-radius: 20px;
   :hover ${Overlay} {
-    opacity: 1;
+    opacity: 0.7;
   }
 `;
 
 const Text = styled.div`
-  color: white;
+  color: ${({ theme }) => theme.colors.dark};
   font-size: 20px;
   position: absolute;
   top: 50%;
@@ -118,24 +81,26 @@ const Header = () => {
     <BodyWrapper>
       {result.map((book) => (
         <Book>
-          <Cont>
-            <Img
-              src={
-                book.volumeInfo.imageLinks
-                  ? book.volumeInfo.imageLinks.thumbnail
-                  : nobook
-              }
-              alt="Avatar"
-              class="image"
-            />
-            <Overlay>
-              <Text>
-                {book.volumeInfo.title.length > 50
-                  ? book.volumeInfo.title.replace(/ .*/, "")
-                  : book.volumeInfo.title}
-              </Text>
-            </Overlay>
-          </Cont>
+          <a target="_blank" href={book.volumeInfo.previewLink}>
+            <Cont>
+              <Img
+                src={
+                  book.volumeInfo.imageLinks
+                    ? book.volumeInfo.imageLinks.thumbnail
+                    : nobook
+                }
+                alt="Avatar"
+                class="image"
+              />
+              <Overlay>
+                <Text>
+                  {book.volumeInfo.title.length > 50
+                    ? book.volumeInfo.title.replace(/ .*/, "")
+                    : book.volumeInfo.title}
+                </Text>
+              </Overlay>
+            </Cont>
+          </a>
         </Book>
       ))}
     </BodyWrapper>

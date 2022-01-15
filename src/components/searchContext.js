3b413845f2bec058bd0565e86b0fe144630a1a57
Your Name <patrykkubala1999@gmail.com>
2022-01-15 20:12:29 +0100

    fixed errors

--- src/components/searchContext.js
+++ src/components/searchContext.js
@@ -5,7 +5,7 @@ const SearchContext = React.createContext();
 function SearchContextProvider(props) {
   const [search, setSearch] = useState();
   const [result, setResult] = useState([]);
-  const [apiKey] = useState("AIzaSyC7kSGBBwaEW_cAoCAtnEnlh3vPRgIWoaA");
+  const [apiKey] = useState("AIzaSyDj6kO6GZh6r47FtDx78bpoeR1pfTUGBAM");
 
   //function toggleTheme() {
   //     setTheme(prevTheme => prevTheme === "light" ? "dark" : "light")
